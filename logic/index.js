db6cb1c187cea623555884cc377917d14edb9e22
theparash <parashphuyal089@gmail.com>
2023-10-08 13:15:22 +0545

    Add keyboard navigation
    Implement keyboard navigation for anchor tags
    
    This commit introduces keyboard navigation enhancements to improve accessibility. Specifically, it adds event listeners to anchor tags within the document. Users can now navigate to and activate these anchors using keyboard shortcuts corresponding to their respective indices. For example, pressing the '1' key will trigger a click on the first anchor, '2' for the second anchor, and so on. Additionally, an event listener for the 'Escape' key has been added to simulate clicks on the "Back" anchor when 'Esc' is pressed. These changes aim to provide a smoother user experience and comply with accessibility standards.

--- logic/index.js
+++ logic/index.js
@@ -12,3 +12,61 @@ var typed = new Typed("#element", {
   backSpeed: 50,
   backDelay: 700,
 });
+
+// Function to handle key presses
+function handleKeyPress(event) {
+  if (
+    event.key === "1" ||
+    event.key === "2" ||
+    event.key === "3" ||
+    event.key === "4"
+  ) {
+    // Get the index of the key pressed ('1' corresponds to index 0, '2' to index 1, and '3' to index 2)
+    const index = parseInt(event.key) - 1;
+
+    const anchors = document.querySelectorAll(".link.body-links");
+
+    if (index >= 0 && index < anchors.length) {
+      anchors[index].click();
+    }
+  }
+}
+
+// Add the key press event listener to the document
+document.addEventListener("keydown", handleKeyPress);
+
+// Function to handle "Esc" key press
+function handleEscKeyPress(event) {
+  if (event.key === "Escape") {
+    // Get a reference to the anchor tag
+    const anchor = document.getElementById("name");
+
+    if (anchor) {
+      anchor.click();
+    }
+  }
+}
+
+// Add the key press event listener to the document
+document.addEventListener("keydown", handleEscKeyPress);
+
+// Function to handle "Back" anchor click
+function handleBackAnchorClick() {
+  // Get a reference to the anchor tag with the ID 'name'
+  const anchor = document.getElementById("name");
+
+  // Check if the anchor tag exists
+  if (anchor) {
+    // Simulate a click on the anchor tag
+    anchor.click();
+  }
+}
+
+// Add a click event listener to the "Back" anchor tag
+document.addEventListener("keydown", function (event) {
+  // Check if the key pressed is the "Enter" key (key code 13)
+  if (event.key === "Escape") {
+    // Call the function to handle the anchor click
+    handleBackAnchorClick();
+  }
+});
