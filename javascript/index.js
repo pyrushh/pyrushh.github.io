a478c32c03922aeccfd20eade27f437196d154cf
pyrushh <parashphuyal089@gmail.com>
2023-12-08 21:22:01 +0545

    slight redesign

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,4 @@
-var typed = new Typed("#element", {
-  strings: [
-    "Hello..",
-    " Namaste...",
-    "Bonjour...",
-    "Nǐn hǎo...",
-    "Konnichiwa...",
-    "Hola...",
-  ],
-  typeSpeed: 70,
-  loop: true,
-  backSpeed: 50,
-  backDelay: 700,
-});
-
-// Function to handle key presses
+// Function to open the link related to the numbers
 function handleKeyPress(event) {
   if (
     event.key === "1" ||
@@ -21,20 +6,15 @@ function handleKeyPress(event) {
     event.key === "3" ||
     event.key === "4"
   ) {
-    // Get the index of the key pressed ('1' corresponds to index 0, '2' to index 1, and '3' to index 2)
     const index = parseInt(event.key) - 1;
-
     const anchors = document.querySelectorAll(".link.body-links");
-
     if (index >= 0 && index < anchors.length) {
       anchors[index].click();
     }
   }
 }
-
 // Add the key press event listener to the document
 document.addEventListener("keydown", handleKeyPress);
-
 // Function to handle "backspace" key press
 function handleBackspaceKeyPress(event) {
   if (event.key === "Backspace") {
@@ -45,24 +25,18 @@ function handleBackspaceKeyPress(event) {
     }
   }
 }
-
 // Add the key press event listener to the document
 document.addEventListener("keydown", handleBackspaceKeyPress);
-
 // Function to handle "Back" anchor click
 function handleBackAnchorClick() {
   const anchor = document.getElementById("name");
-
   if (anchor) {
-    // Simulate a click on the anchor tag
     anchor.click();
   }
 }
-
 // Add a click event listener to the "Back" anchor tag
 document.addEventListener("keydown", function (event) {
   if (event.key === "Backspace") {
-    // Call the function to handle the anchor click
     handleBackAnchorClick();
   }
 });
